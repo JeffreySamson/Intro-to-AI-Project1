53d3819f26e7d60600724ab0028e3fe85d72ab29
notdavidjoseph <2davidjoseph@gmail.com>
2021-02-04 15:01:34 -0500

    Visual Maze

--- maze.py
+++ maze.py
@@ -1,38 +1,45 @@
+
+from tkinter import *
+import numpy as np
 import random
 
-print()  # prints a empty row
 
-dim = int(input('Enter the size of the array: '))
-prob = float(input('Enter the probability of an element being a 1 or 0: '))
+DIM = int(input('Enter the size of the array: '))
+PROB = float(input('Enter the probability of an element being a 1 or 0: '))
+SIZE = DIM**2
+GRID = np.ones(DIM**2)
 
-grid = [ [1]*dim for n in range(dim)]
-for row in grid:
-    print(row)
+class makeGrid():
+    numEmpty = (SIZE)*PROB
+    numEmpty = int(round(numEmpty))
 
-numEmpty = (dim**2)*prob
-numEmpty = int(round(numEmpty))
-#print(numEmpty)
+    c = 0
+    while c < numEmpty :
+        temp = random.randint(0,SIZE-1)
 
-c = 0
-#
-while c < numEmpty :
-    pos1 = random.randint(0,dim-1)
-    pos2 = random.randint(0,dim-1)
+        if (GRID[temp] == 1):
+            GRID[temp] = 0
+            c +=1
 
-    temp = grid[pos1][pos2]
+class checkerBoard():
+    def __init__(self):
+        window = Tk()
+        window.title("Fire Maze")
 
-    if (temp == 1):
-        grid[pos1][pos2] = 0
-        c +=1
-
-
-print() #prints an empty row
-
-for row in grid:
-    print(row)
-
-    
+        for i in range(SIZE):
+            if(GRID[i] == 1):
+                color = "white"
+            else :
+                color = "black"
+            Canvas(window, width=50, height = 50, bg = color).grid(row = i // DIM, column = i % DIM)
+        
 
 
 
-print() #prints an empty row
+        numEmpty = (DIM**2)*PROB
+        numEmpty = int(round(numEmpty))
+        
+        window.mainloop()
+
+makeGrid()
+checkerBoard()    
