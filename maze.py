11812bd555da27384006441eeb1256e234c6b3f6
notdavidjoseph <2davidjoseph@gmail.com>
2021-02-16 17:12:43 -0500

    Fixed branch

--- maze.py
+++ maze.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 from typing import TYPE_CHECKING
 import numpy as np
@@ -8,6 +7,11 @@ from queue import Empty, PriorityQueue
 import heapq
 from collections import deque
 
+# 0: blocked
+# -1: start
+# -2: end
+# 7: path
+# -3: fire
 
 def main():
 
@@ -16,27 +20,20 @@ def main():
     global DIM
     global GRID
     global PROB
-    global burnRate
     global start
     global end
+    global q
 
-    #DIM = int(input('Enter the size of the array\n')) # Takes in user input for maze dimesnions
-    #PROB = float(input('Enter the probability of an element being a 1 or 0\n')) # Takes in user input for probability of blocked spaces
+    #DIM = int(input('Enter the size of the array: '))
+    #PROB = float(input('Enter the probability of an element being a 1 or 0: '))
     #while (PROB <0 or PROB >1):
         #print ('PROB must be between 1 and 0')
-        #PROB = float(input('Enter the probability of an element being a 1 or 0\n'))
-    burnRate = float(input('Enter the probability of an free space getting burned\n'))
-    while (burnRate <0 or burnRate >1):
-        print ('PROB must be between 1 and 0')
-        burnRate = float(input('Enter the probability of an free space getting burned\n'))
-    #DIM = 10
-    #PROB = 0.3
-    
+        #PROB = float(input('Enter the probability of an element being a 1 or 0: '))
 
-    searchType = input('How would you like to search for a path? (Enter BFS/DFS or Astar)\n').casefold()
-    debug = input('Would you like debugging print statements? (Enter y/n)\n').casefold()
-    DIM = 20
-    PROB =0.1
+    DIM = 10
+    PROB = 0.2
+    q = 0
+
     SIZE = DIM**2
 
     # generates random points to start
@@ -56,18 +53,13 @@ def main():
     # runing the initial grid
     makeGrid()
     # shows how the initial grid looks like
-    checkerBoard()
+    #checkerBoard()
 
     # runs search
-    if searchType == 'dfs':
-        solution = DFS(start, end)
-    elif searchType == 'bfs':
-        solution = BFS(start,end)
-    else:
-        solution = AStar(start, end)
-    #solution = AStar(start, end)
-    print(solution)
+    solution = AStar(start, end)
+    #print(solution)
 
+    print(solution)
 
     # checks if there is a solution and then marks the path
     if solution:
@@ -80,37 +72,43 @@ def main():
     print() #prints an empty line
     # generates the final solution
 
-    checkerBoard()
-
-    #solution2 = BFS(start,end)
-
-    #print("Astar: {} BFS: {}".format(len(solution),len(solution)))
+    showMaze()
 
 
-
+    # call BFS
+    # make one step
+    # call adv fire one step
     
+    #call bfs 
+    # make one step
+    # call adv fire one step
 
 
 
 
 ######################[functions]######################
 
-def burnGrid(GRID, start, end):
-    global burnRate
-    global SIZE
-    # Make a copy of the maze
-    newGrid = np.copy(GRID)
-    for node in range(len(GRID)):
-        if newGrid[node] != 0 and newGrid[node] != -3 and node != start and node != end:
-            neighbors = getBurnedNeighbors(node)
-            i = 0
-            for j in neighbors:
-                if(GRID[j] == -3):
-                    i +=1
-            prob = 1 - ((1-burnRate)**i)
-            if random.uniform(0, 1) <= prob:
-                newGrid[node] = -3
-    return newGrid
+def advFireOneStep():
+    global q
+    global GRID
+
+    mazeCopy = GRID
+
+    for current in mazeCopy :
+        if not Grid[current] == -3 and not Grid[current] ==0:
+            neighbors = getFireNeighbors(current)
+            k = len(neighbors)
+            prob = 1-(1-q)**k
+            if random() <= prob:
+                GRID[current] = -3
+        
+
+
+
+
+
+
+    return 
 
 def AStar(start,end):
     
@@ -131,11 +129,12 @@ def AStar(start,end):
     while fringe: # checks if the fringe is empty
         
         (d,v,path) = heapq.heappop(fringe)
+        #print(fringe)
 
         # found the path
         if (v == end):
             print ("Success!")
-            return [v] + path
+            return path
         
         if not processed[v]:
 
@@ -145,17 +144,14 @@ def AStar(start,end):
                 if ((d + Heu(u)) < dist[u]):
                     dist[u] = d + Heu(u)
                     #print('going to {} from {} is {}'.format(v,u,dist[u]))
-                    heapq.heappush(fringe,(dist[u],u,[u]+path))
+                    heapq.heappush(fringe,(dist[u],u,path + [u]))
                     prev[u] = v
             processed[v] = True
 
     print("No path")
     return None
 
-
-
-
-# eucledian heuristic
+# eucledian heuristic for AStar
 def Heu(current):
     global DIM
     global end
@@ -172,9 +168,6 @@ def Heu(current):
     dis = math.sqrt(((x-endX)**2) + (y-endY)**2)
 
     return dis
-
-
-
 
 #executes the BFS algorithm
 def BFS(start,end):
@@ -238,7 +231,7 @@ def DFS(start, end):
 
     while fringe: # checks if the fringe is empty
 
-        # pops the top off the stack -> Agent Moves
+        # pops the top off the stack
         current = fringe.pop()
         path.append(current)
         #print("current {}".format(current))
@@ -248,7 +241,6 @@ def DFS(start, end):
             print ("Success!")
             return path
         # calculate the valid neighbors
-        GRID = burnGrid(GRID, start, end)
         vldneigh = getNeighbors(current)
         #print('Valid Neighbors for {} is {}'.format(current,vldneigh))
 
@@ -259,8 +251,7 @@ def DFS(start, end):
                 #path.append(current)
         closedSet.append(current)
     print("No Solution ")
-    return path
-        
+    return None
     
 # makes the grid using the probability and the size
 def makeGrid():
@@ -274,9 +265,6 @@ def makeGrid():
 
     numEmpty = (SIZE)*PROB
     numEmpty = int(round(numEmpty))
-    # Adding spaces already on fire 
-    burningSpaces = (SIZE)*burnRate
-    burningSpaces = int(round(burningSpaces))
 
     GRID[start] = -1
     GRID[end] = -2
@@ -289,16 +277,8 @@ def makeGrid():
             GRID[temp] = 0
             c +=1
 
-    # Loop to mark the burning spaces 
-    c = 0
-    while c < burningSpaces:
-        temp = random.randint(0,SIZE-1)
-        if (GRID[temp] == 1 and not(temp ==0) and not(temp == SIZE -1)):
-            GRID[temp] = -3
-            c +=1
-
 # makes the visual canvas for the grid
-class checkerBoard():
+class showMaze():
     def __init__(self):
         
         # makes the window for the maze
@@ -315,8 +295,6 @@ class checkerBoard():
                 color = "blue" # start is blue
             elif(GRID[i] == -2):
                 color = "purple" # end is purple
-            elif(GRID[i] == -3):
-                color = "red" # burning spaces
             else:
                 color = "green" # path taken
             Canvas(window, width=30, height = 30, bg = color).grid(row = i // DIM, column = i % DIM)
@@ -353,12 +331,14 @@ def getNeighbors(current):
         tempNeighbors.remove(up)
     # gets rid of all the nieghbors that are empty
     for i in tempNeighbors:
-        if not (GRID[i] == 0 or GRID[i] == -3):
+        if (not GRID[i] == 0) and not Grid[i] ==-3):
             neighbors.append(i)
 
     return neighbors
 
-def getBurnedNeighbors(current):
+#gets the Neighbors that are on fire
+def getFireNeighbors(current):
+    
     global SIZE
     global GRID
     global DIM
@@ -370,6 +350,9 @@ def getBurnedNeighbors(current):
 
 
     tempNeighbors = [ up, down, right, left] # all possible neighbors
+    FireNeighbors = [] # valid neighbors
+
+    #checks if the current is on the left edges and gets rid of left neighbor
     if (current % DIM == 0):
         tempNeighbors.remove(left)
     #checks if the current is on the right edges and gets rid of right neighbor
@@ -377,16 +360,18 @@ def getBurnedNeighbors(current):
         tempNeighbors.remove(right)
     #checks if the current is on the down edge and gets rid of down neighbor
     if (current // DIM == (DIM -1)):
-        tempNeighbors.remove(down)
+        tempNeighbors.remove(down -- )
     #checks if the current is on the top edge and gets rid of top neighbor
     elif(current //DIM == 0):
         tempNeighbors.remove(up)
-    return tempNeighbors
+    # gets rid of all the nieghbors that are empty
+    for i in tempNeighbors:
+        if (Grid[i] ==-3):
+            FireNeighbors.append(i)
+
+    return FireNeighbors
+
 
 #___________________________________________________________________
 # RUN THE MAIN: DO NOT DELETE!
 if __name__ == '__main__': main()
-
-
-
-
