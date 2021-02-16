6bcadc27ed1c01145c09abbb73d2bb603ff0147a
jeffrey.s <jeffreys@live.com>
2021-02-16 16:22:49 -0500

    Adding burn function - Jeff

--- maze.py
+++ maze.py
@@ -16,17 +16,27 @@ def main():
     global DIM
     global GRID
     global PROB
+    global burnRate
     global start
     global end
 
-    #DIM = int(input('Enter the size of the array: '))
-    #PROB = float(input('Enter the probability of an element being a 1 or 0: '))
+    #DIM = int(input('Enter the size of the array\n')) # Takes in user input for maze dimesnions
+    #PROB = float(input('Enter the probability of an element being a 1 or 0\n')) # Takes in user input for probability of blocked spaces
     #while (PROB <0 or PROB >1):
         #print ('PROB must be between 1 and 0')
-        #PROB = float(input('Enter the probability of an element being a 1 or 0: '))
+        #PROB = float(input('Enter the probability of an element being a 1 or 0\n'))
+    burnRate = float(input('Enter the probability of an free space getting burned\n'))
+    while (burnRate <0 or burnRate >1):
+        print ('PROB must be between 1 and 0')
+        burnRate = float(input('Enter the probability of an free space getting burned\n'))
+    #DIM = 10
+    #PROB = 0.3
+    
 
-    DIM = 200
-    PROB =0.3
+    searchType = input('How would you like to search for a path? (Enter BFS/DFS or Astar)\n').casefold()
+    debug = input('Would you like debugging print statements? (Enter y/n)\n').casefold()
+    DIM = 20
+    PROB =0.1
     SIZE = DIM**2
 
     # generates random points to start
@@ -46,11 +56,17 @@ def main():
     # runing the initial grid
     makeGrid()
     # shows how the initial grid looks like
-    #checkerBoard()
+    checkerBoard()
 
     # runs search
-    solution = AStar(start, end)
-    #print(solution)
+    if searchType == 'dfs':
+        solution = DFS(start, end)
+    elif searchType == 'bfs':
+        solution = BFS(start,end)
+    else:
+        solution = AStar(start, end)
+    #solution = AStar(start, end)
+    print(solution)
 
 
     # checks if there is a solution and then marks the path
@@ -64,7 +80,7 @@ def main():
     print() #prints an empty line
     # generates the final solution
 
-    #checkerBoard()
+    checkerBoard()
 
     #solution2 = BFS(start,end)
 
@@ -78,6 +94,23 @@ def main():
 
 
 ######################[functions]######################
+
+def burnGrid(GRID, start, end):
+    global burnRate
+    global SIZE
+    # Make a copy of the maze
+    newGrid = np.copy(GRID)
+    for node in range(len(GRID)):
+        if newGrid[node] != 0 and newGrid[node] != -3 and node != start and node != end:
+            neighbors = getBurnedNeighbors(node)
+            i = 0
+            for j in neighbors:
+                if(GRID[j] == -3):
+                    i +=1
+            prob = 1 - ((1-burnRate)**i)
+            if random.uniform(0, 1) <= prob:
+                newGrid[node] = -3
+    return newGrid
 
 def AStar(start,end):
     
@@ -205,7 +238,7 @@ def DFS(start, end):
 
     while fringe: # checks if the fringe is empty
 
-        # pops the top off the stack
+        # pops the top off the stack -> Agent Moves
         current = fringe.pop()
         path.append(current)
         #print("current {}".format(current))
@@ -215,6 +248,7 @@ def DFS(start, end):
             print ("Success!")
             return path
         # calculate the valid neighbors
+        GRID = burnGrid(GRID, start, end)
         vldneigh = getNeighbors(current)
         #print('Valid Neighbors for {} is {}'.format(current,vldneigh))
 
@@ -240,6 +274,9 @@ def makeGrid():
 
     numEmpty = (SIZE)*PROB
     numEmpty = int(round(numEmpty))
+    # Adding spaces already on fire 
+    burningSpaces = (SIZE)*burnRate
+    burningSpaces = int(round(burningSpaces))
 
     GRID[start] = -1
     GRID[end] = -2
@@ -250,6 +287,14 @@ def makeGrid():
 
         if (GRID[temp] == 1 and not(temp ==0) and not(temp == SIZE -1)):
             GRID[temp] = 0
+            c +=1
+
+    # Loop to mark the burning spaces 
+    c = 0
+    while c < burningSpaces:
+        temp = random.randint(0,SIZE-1)
+        if (GRID[temp] == 1 and not(temp ==0) and not(temp == SIZE -1)):
+            GRID[temp] = -3
             c +=1
 
 # makes the visual canvas for the grid
@@ -270,6 +315,8 @@ class checkerBoard():
                 color = "blue" # start is blue
             elif(GRID[i] == -2):
                 color = "purple" # end is purple
+            elif(GRID[i] == -3):
+                color = "red" # burning spaces
             else:
                 color = "green" # path taken
             Canvas(window, width=30, height = 30, bg = color).grid(row = i // DIM, column = i % DIM)
@@ -306,10 +353,35 @@ def getNeighbors(current):
         tempNeighbors.remove(up)
     # gets rid of all the nieghbors that are empty
     for i in tempNeighbors:
-        if (not GRID[i] == 0):
+        if not (GRID[i] == 0 or GRID[i] == -3):
             neighbors.append(i)
 
     return neighbors
+
+def getBurnedNeighbors(current):
+    global SIZE
+    global GRID
+    global DIM
+
+    left = current -1
+    right = current+1
+    up = current - DIM
+    down = current + DIM
+
+
+    tempNeighbors = [ up, down, right, left] # all possible neighbors
+    if (current % DIM == 0):
+        tempNeighbors.remove(left)
+    #checks if the current is on the right edges and gets rid of right neighbor
+    elif (current % DIM == (DIM -1)):
+        tempNeighbors.remove(right)
+    #checks if the current is on the down edge and gets rid of down neighbor
+    if (current // DIM == (DIM -1)):
+        tempNeighbors.remove(down)
+    #checks if the current is on the top edge and gets rid of top neighbor
+    elif(current //DIM == 0):
+        tempNeighbors.remove(up)
+    return tempNeighbors
 
 #___________________________________________________________________
 # RUN THE MAIN: DO NOT DELETE!
